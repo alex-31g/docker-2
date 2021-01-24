b526e491154b47672eb0ab8324939347cfe86fa1
alex-31g <al.tsimbalyuk@gmail.com>
2021-01-24 17:51:56 +0200

    add

--- auth/src/index.js
+++ auth/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
+const axios = require("axios");
 const { connectDB } = require("./helpers/db");
-const { host, port } = require("./configuration");
+const { host, port, apiUrl } = require("./configuration");
 const app = express();
 
 const startServer = () => {
@@ -22,10 +23,22 @@ app.get("/test", (req, res) => {
   res.send("auth server is working");
 });
 
+// Обращение к этому url будет происходить из api-сервиса
 // Возвращаем пользователя
 app.get("/api/currentUser", (req, res) => {
   res.json({
     id: "1234",
     email: "foo@gmail.com",
+  });
+});
+
+// Когда возникает запрос http://localhost:3002/testwithapidata к auth-сервису
+// для получения данных, сперва нужно выполнить запрос к api-сервису,
+// дождаться его ответа, и потом возвратить данные
+app.get("/testwithapidata", (req, res) => {
+  axios.get(apiUrl + "/testapidata").then((response) => {
+    res.json({
+      testapidata: response.data.testwithapi,
+    });
   });
 });
